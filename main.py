da46629a0ba2b953340e17ec3472ebe8d7f78f98
corailllum <160240585+corailllum@users.noreply.github.com>
2024-04-16 08:52:09 +0200

    finistion de la fonction de lecture

--- main.py
+++ main.py
@@ -18,26 +18,51 @@ def lectureFichierBinaire(nomfichier):
 fichier_binaire =(lectureFichierBinaire('exemple_comp.bin'))
 tabCharactereDutexte=[]
 tabIterationTexte=[]
+nbCaractere=0
 dicoalphabets={}
 def lectureFichierTexte(nomfichier):
     """fonction qui permet de lire et recuprée les information du fichier alphabets 
     entré: nom du fichier alphabets str
-    sortie: str
+    sortie: str dictionnaire/listes de l'alphabets 
     """
     with open(nomfichier,'r') as f:
+        #lecture et recuperation du nombre total de caractere
+        data=f.readline()
+        nbCaractere=data[0]
+        data=f.readline()
+        print(data)
+        while(data!=""):
+            print(42)
+            print(data)
+            tabCharactereDutexte.append(data[0])
+            tabIterationTexte.append(data[2])
+            dicoalphabets[data[0]]=data[2]
+            #tabIterationTexte.append(data[2])
+            data=f.readline()
+        f.close()    
+        """
         data=f.readline()
         data=f.readline()
+        tabCharactereDutexte.append(data[0])
         data=f.readline()
+        tabCharactereDutexte.append(data[0])
         data=f.readline()
+        tabCharactereDutexte.append(data[0])
         data=f.readline()
+        tabCharactereDutexte.append(data[0])
         data=f.readline()
+        tabCharactereDutexte.append(data[0])
         data=f.readline()
+        tabCharactereDutexte.append(data[0])
         data=f.readline()
+        tabCharactereDutexte.append(data[0])
         
-        
-        tabCharactereDutexte.append(data)
-        f.close()
+        print(tabCharactereDutexte)
+        f.close()"""
     return
 
 lectureFichierTexte('exemple_freq.txt')
+print(dicoalphabets)
 print(tabCharactereDutexte)
+print(tabIterationTexte)
+print(fichier_binaire)
